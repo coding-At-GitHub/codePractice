e38c0f9de7067dcd82ea0df305417553636a1d8f
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2023-12-27 10:27:07 +0000

    route

---
+++ GoodCodeStructurePractice/route/allroute.tsx
@@ -0,0 +1,18 @@
+
+export const appRoutes = [
+ 
+    {
+        path: "login",
+        title: "Login",
+        component: Login,
+        show: true,
+    },
+    {
+        path: "home",
+        title: "Home",
+        component: Home,
+        show: true,
+    },
+ 
+   
+]
