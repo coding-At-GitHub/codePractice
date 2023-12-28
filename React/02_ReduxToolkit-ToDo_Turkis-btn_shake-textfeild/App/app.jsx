599ad8763d070a8842008ad2741d1b9773b507ce
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2023-12-28 06:11:43 +0000

    rtk-todo

---
+++ React/02_ReduxToolkit-ToDo_Turkis-btn_shake-textfeild/App/app.jsx
@@ -0,0 +1,20 @@
+import TodoItem from "./components/TodoItem"
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+function App() {
+
+  return (
+    <>
+ {/* todo shake turkis button  */}
+      <AddTodo />
+<Todos />
+      <TodoItem />
+
+   
+
+    </>
+  )
+}
+
+export default App
